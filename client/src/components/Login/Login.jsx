46a48ab9fb169aaa8d47726279267d2f37f54600
adrianrakiec <rakiecadrian17@gmail.com>
2024-10-03 16:23:11 +0200

    feat: add basic login logic with react-hook-form

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,15 +1,47 @@
+import { useForm } from 'react-hook-form';
+
 export const Login = () => {
+	const { register, handleSubmit, formState } = useForm();
+
+	const onSubmit = data => {
+		console.log(data);
+	};
+
 	return (
 		<div>
 			<h3>Login</h3>
-			<form autoComplete='off'>
+			<form onSubmit={handleSubmit(onSubmit)} autoComplete='off'>
 				<div>
-					<label htmlFor="username">Login:</label>
-					<input id="username" type='text' required />
+					<label htmlFor='username'>Login:</label>
+					<input
+						id='username'
+						type='text'
+						{...register('username', {
+							required: {
+								value: true,
+								message: 'Nazwa użytkownika jest wymagana!',
+							},
+						})}
+					/>
+					{formState.errors.username && (
+						<span>{formState.errors.username.message}</span>
+					)}
 				</div>
 				<div>
-					<label htmlFor="password">Hasło:</label>
-					<input id="password" type='password' required />
+					<label htmlFor='password'>Hasło:</label>
+					<input
+						id='password'
+						type='password'
+						{...register('password', {
+							required: {
+								value: true,
+								message: 'Hasło jest wymagane!',
+							},
+						})}
+					/>
+					{formState.errors.password && (
+						<span>{formState.errors.password.message}</span>
+					)}
 				</div>
 				<button type='submit'>Zaloguj się</button>
 			</form>
