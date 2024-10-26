0b117635475b1e1cc7911e62ff3889e33b3719b2
adrianrakiec <rakiecadrian17@gmail.com>
2024-10-26 17:14:53 +0200

    feat: add react router and first path

--- client/src/App.jsx
+++
@@ -1,5 +0,0 @@
-import { MainPage } from './pages/MainPage';
-
-export const App = () => {
-	return <MainPage />;
-};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { App } from './App';
+import { AuthProvider } from './context/AuthContext';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { MainPage } from './pages/MainPage';
 
 import './index.css';
-import { AuthProvider } from './context/AuthContext';
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+	{
+		element: <MainPage />,
+		path: '/',
+	},
+]);
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<AuthProvider>
-				<App />
+				<RouterProvider router={router} />
 			</AuthProvider>
 		</QueryClientProvider>
 	</StrictMode>
