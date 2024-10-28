40481edcd46e3d876a0be9ae59390ff779aca2fa
adrianrakiec <rakiecadrian17@gmail.com>
2024-10-28 17:34:10 +0100

    extend: extend User entity and add migration

--- API/Entities/User.cs
+++ API/Entities/User.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities;
 
 public class User
 {
     public int Id { get; set; }
     public required string UserName { get; set; }
-    public required byte[] PasswordHash { get; set; }
-    public required byte[] PasswordSalt { get; set; }
+    public byte[] PasswordHash { get; set; } = [];
+    public byte[] PasswordSalt { get; set; } = [];
+    [DataType(DataType.DateTime)]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    [DataType(DataType.DateTime)]
+    public DateTime LastActive { get; set; } = DateTime.Now;
+    [Range(1, 5)]
+    public double? Rating { get; set; }
+    public string? About { get; set; }
+    public List<Photo>? ProfilePhoto { get; set; }
 }
