589863dc4cb2c98dc8efabcea368bf55e4dadbb3
adrianrakiec <rakiecadrian17@gmail.com>
2024-10-28 18:12:07 +0100

    feat: add seed data to database

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
+using API.Data;
 using API.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +25,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try 
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUser(context);
+}
+catch(Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "Błąd podczas migracji");
+}
 
 app.Run();
