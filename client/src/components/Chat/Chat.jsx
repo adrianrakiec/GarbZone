bb323923505c9dcc589b9994a3279be3ef1436c8
adrianrakiec <rakiecadrian17@gmail.com>
2024-12-18 14:39:43 +0100

    feat: add user profile link and validation for username existence in Chat component

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,13 +1,19 @@
-import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { useFetchData } from '../../services/ApiClientService';
+import { AuthContext } from '../../context/AuthContext';
 import { Wrapper } from '../Wrapper/Wrapper';
 import { ChatMessage } from '../ChatMessage/ChatMessage';
 import { HorizontalRule } from '../HorizontalRule/HorizontalRule';
 import { ChatMessageForm } from '../ChatMessageForm/ChatMessageForm';
+import { NotFoundPage } from '../../pages/NotFoundPage';
 import styles from './Chat.module.css';
 
 export const Chat = () => {
 	const { username } = useParams();
+	const { user: loggedUser } = useContext(AuthContext);
+	const { data: user } = useFetchData(`users/${username}`, ['user']);
+
 	const { data, refetch } = useFetchData(
 		`messages/thread/${username}`,
 		['messages', username],
@@ -15,15 +21,22 @@ export const Chat = () => {
 		true
 	);
 
+	if (!user || loggedUser === username) return <NotFoundPage />;
+
 	const messages = data?.data;
+	const linkToProfile = `/profil/${username}`;
 
 	return (
 		<>
 			<Wrapper>
 				<section className={styles.chat}>
 					<h2>
-						Piszesz z użytkownikiem <span>{username}</span>
+						Piszesz z użytkownikiem{' '}
+						<Link to={linkToProfile}>
+							<span className={styles.profileLink}>{username}</span>
+						</Link>
 					</h2>
+
 					<div className={styles.messages}>
 						<div className={styles.msgContainer}>
 							{messages?.length > 0 ? (
